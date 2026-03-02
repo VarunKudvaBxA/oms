8dcc486d1651970b655c9dcf20afc3f507e5a314
VarunKudvaBxA <Varun.Kudva@bounteous.com>
2026-03-02 12:07:41 +0530

    Update OrderFileLogger.java

--- oms/src/main/java/com/example/oms/util/OrderFileLogger.java
+++ oms/src/main/java/com/example/oms/util/OrderFileLogger.java
@@ -1,18 +1,18 @@
 package com.example.oms.util;
 import com.example.oms.model.Order;
 import org.springframework.stereotype.Component;
+import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
-
 @Component
 public class OrderFileLogger {
-
+    private static final String FILE_PATH = "orders.log";
     public void logOrder(Order order) {
-        try (FileWriter writer = new FileWriter("orders.log", true)) {
-            writer.write(order.toString() + System.lineSeparator());
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(FILE_PATH, true))) {
+            writer.write(order.toString());
+            writer.newLine();
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 }
-
