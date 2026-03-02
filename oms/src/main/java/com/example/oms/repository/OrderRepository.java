ca78f6e88b4bfeedd65c0dc58860b7e3500a9454
VarunKudvaBxA <Varun.Kudva@bounteous.com>
2026-03-02 12:05:52 +0530

    Update OrderRepository.java

--- oms/src/main/java/com/example/oms/repository/OrderRepository.java
+++ oms/src/main/java/com/example/oms/repository/OrderRepository.java
@@ -1,18 +1,14 @@
 package com.example.oms.repository;
 import com.example.oms.model.Order;
 import org.springframework.stereotype.Repository;
-import java.util.ArrayList;
 import java.util.List;
-
+import java.util.concurrent.CopyOnWriteArrayList;
 @Repository
 public class OrderRepository {
-
-    private final List<Order> orders = new ArrayList<>();
-
+    private final List<Order> orders = new CopyOnWriteArrayList<>();
     public void save(Order order) {
         orders.add(order);
     }
-
     public List<Order> findAll() {
         return orders;
     }
